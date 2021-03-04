d8a09d59f31c962d8f7a861e8e45e3bfb0e5cf38
Kauan Nascimento <72675422+K1Nascimento@users.noreply.github.com>
2021-03-04 18:51:52 -0300

    Update classepontoequadrilatero.py

--- classepontoequadrilatero.py
+++ classepontoequadrilatero.py
@@ -21,3 +21,16 @@ class Ponto:
             return 4
         if (y == 0 and x == 0):
             return
+        class Quadrilatero:
+    def __init__(self, p1,p2): 
+        self.p1 = p1
+        self.p2 = p2
+
+    def verificaQuadrilatero(self): 
+        if self.p1.getX() < self.p2.getX() and self.p1.getY() > self.p2.getY():
+         return True
+        return False
+    def contidoEmQ(self, p3): 
+        if p3.getX() > self.p1.getX() and p3.getX() < self.p2.getX() and p3.getY() < self.p1.getY() and p3.getY() > self.p2.getY():
+         return True
+        return False    
